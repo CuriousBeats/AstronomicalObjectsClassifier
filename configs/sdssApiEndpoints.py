4aa5e56d04aec99e855626131b5c59cc49d147ba
Shaga Sresthaa <shagasresthaa@gmail.com>
2024-03-20 18:32:30 -0400

    added basic configs

--- configs/sdssApiEndpoints.py
+++ configs/sdssApiEndpoints.py
@@ -8,4 +8,14 @@
 # opt = additional options (optional parameter)
 SDSS_IMAGE_CUTOUT_BASE = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?"
 
+# Used for getting the spectra of a given object
+# Takes following parameters
+# id = SpecObjId from SpecObj DB view
+SDSS_SPECTRA_BASE = "http://skyserver.sdss.org/dr18/en/get/specById.ashx?ID="
 
+# Used for searching the SDSS database using SQL
+# Takes following parameters
+# cmd = SQL command to be executed on DB
+# format = To specify the output format (json, csv, fits, xml, html, etc)
+
+SDSS_OBJ_SQL_SEARCH_BASE = "http://skyserver.sdss.org/dr18/SkyServerWS/SearchTools/SqlSearch?"
