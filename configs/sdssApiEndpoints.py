5c3c3739a3cc4be67ee15b18dc3555d79d620b83
Shaga Sresthaa <shagasresthaa@gmail.com>
2024-03-20 16:47:19 -0400

    added configs directory and working on fetch scripts

---
+++ configs/sdssApiEndpoints.py
@@ -0,0 +1,11 @@
+# Used for getting the centered image for a given object ID
+# Takes following parameters
+# ra = Right Ascention (in degrees)
+# dec = Declination (in degrees)
+# scale = Scale of image (in arsec per pixel)
+# height = height of image (in pixels)
+# width = width of image (in pixels)
+# opt = additional options (optional parameter)
+SDSS_IMAGE_CUTOUT_BASE = "http://skyserver.sdss.org/dr16/SkyServerWS/ImgCutout/getjpeg?"
+
+
