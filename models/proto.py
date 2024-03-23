ed440a48957953f90d2223626c53100c6ebd9891
Grant <93157942+CuriousBeats@users.noreply.github.com>
2024-03-23 16:43:52 -0400

    Not working on CIFAR

--- models/proto.py
+++ models/proto.py
@@ -24,14 +24,15 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
         self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
         self.pool = nn.MaxPool2d(2, 2)
-        self.fc1 = nn.Linear(32 * 7 * 7, 120) 
-        self.fc2 = nn.Linear(120, 84)
+        self.fc1 = nn.Linear(32 * 8 * 2, 128) 
+        self.fc2 = nn.Linear(128, 84)
         self.fc3 = nn.Linear(84, 10)  
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 32 * 7 * 7) 
+        print(x.shape)
+        x = x.view(-1, 8 * 8 * 8) 
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
